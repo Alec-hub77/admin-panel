a24c2052fd32ee24f95d895e550e22974015bac1
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2022-04-01 10:42:57 +0300

    customers list table and pagination

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -4,6 +4,33 @@ import Table from '../components/table/Table'
 
 import customerList from '../assets/JsonData/customers-list.json'
 
+
+const customerTableHead = [
+  '',
+  'name',
+  'email',
+  'phone',
+  'total orders',
+  'total spend',
+  'location'
+]
+
+const renderHead = (item, index) => (
+  <th key={index}>{item}</th>
+)
+
+const renderBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.name}</td>
+    <td>{item.email}</td>
+    <td>{item.phone}</td>
+    <td>{item.total_orders}</td>
+    <td>{item.total_spend}</td>
+    <td>{item.location}</td>
+  </tr>
+)
+
 const Customers = () => {
   return (
     <div>
@@ -14,7 +41,13 @@ const Customers = () => {
         <div className="col-12">
           <div className="card">
             <div className="card_body">
-
+              <Table
+                limit='10'
+                headData={customerTableHead}
+                renderHead={(item, index) => renderHead(item, index)}
+                bodyData={customerList}
+                renderBody={(item,index) => renderBody(item, index)}
+              />
             </div>
           </div>
         </div>
